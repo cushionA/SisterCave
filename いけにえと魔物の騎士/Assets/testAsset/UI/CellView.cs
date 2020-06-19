b4be3206b1430c45e304cdca132ac113b2e5e178
cushionA <42813274+cushionA@users.noreply.github.com>
2020-06-20 02:10:43 +0900

    UI仕様変更

--- いけにえと魔物の騎士/Assets/testAsset/UI/CellView.cs
+++ いけにえと魔物の騎士/Assets/testAsset/UI/CellView.cs
@@ -4,8 +4,14 @@ using EnhancedUI.EnhancedScroller;
 using EnhancedUI;
 using System;
 
-namespace EnhancedScrollerDemos.GridSimulation
+namespace EnhancedScrollerDemos.GridSelection
 {
+    /// <summary>
+    /// This delegate handles the UI's button click
+    /// </summary>
+    /// <param name="cellView">The cell view that had the button click</param>
+    public delegate void SelectedDelegate(RowCellView rowCellView);
+
     /// <summary>
     /// This is the view of our cell which handles how the cell looks.
     /// It stores references to sub cells
@@ -18,14 +24,16 @@ namespace EnhancedScrollerDemos.GridSimulation
         /// This function just takes the Demo data and displays it
         /// </summary>
         /// <param name="data"></param>
-       /* public void SetData(ref SmallList<Data> data, int startingIndex)
+        public void SetData(ref SmallList<Data> data, int startingIndex, SelectedDelegate selected)
         {
             // loop through the sub cells to display their data (or disable them if they are outside the bounds of the data)
             for (var i = 0; i < rowCellViews.Length; i++)
             {
+                var dataIndex = startingIndex + i;
+
                 // if the sub cell is outside the bounds of the data, we pass null to the sub cell
-                rowCellViews[i].SetData(startingIndex + i < data.Count ? data[startingIndex + i] : null);
+                rowCellViews[i].SetData(dataIndex, dataIndex < data.Count ? data[dataIndex] : null, selected);
             }
-        }*/
+        }
     }
 }
